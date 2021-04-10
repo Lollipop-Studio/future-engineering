a262e77e4daf19f5fcabbc4ea8eaae5a5567a960
Konara <imcharlesjiang@163.com>
2021-04-10 21:28:17 +0800

    fix

--- src/main/java/moe/konara/fe/Utils/ItemRegistryHandler.java
+++ src/main/java/moe/konara/fe/Utils/ItemRegistryHandler.java
@@ -1,7 +1,6 @@
 package moe.konara.fe.Utils;
 
 
-import moe.konara.fe.Items.copper_ingot;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
