1bac8887849b95d4a895f5d7ae9d14257b60bd0b
Lucas Liu <bingling_sama@qq.com>
2021-04-11 11:12:32 +0000

    CI test

---
+++ .space.kts
@@ -0,0 +1,17 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build Jar") {
+    container(displayName = "Build Jar", image = "ubuntu") {
+    	shellScript {
+        	content = """
+            	echo Build Start
+            	.\gradlew build
+                echo Build End
+            """
+        }
+    }
+}
