1e500b0de6a78bb820b0a65d038bceff2ab8ea31
Konara <imcharlesjiang@163.com>
2021-04-20 21:57:55 +0800

    generation and map

--- src/main/java/moe/konara/fe/World/OreGen.java
+++ src/main/java/moe/konara/fe/World/OreGen.java
@@ -1,11 +1,18 @@
 package moe.konara.fe.World;
 
+
+import moe.konara.fe.AllBlocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.template.RuleTest;
 import net.minecraft.world.gen.placement.DepthAverageConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.TopSolidRangeConfig;
+import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -13,5 +20,19 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber
 public class OreGen {
-    //TODO 2021年4月18日 矿物生成器
+    //TODO 2021年4月20日 矿物生成器
+    public static void generateOres(final BiomeLoadingEvent event) {
+        if (!(event.getCategory().equals(Biome.Category.NETHER) || event.getCategory().equals(Biome.Category.THEEND))) {
+            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD,
+                AllBlocks.ORE_COPPER.get().getDefaultState(), 5, 1, 60, 8);
+        }
+    }
+
+    private static void generateOre(BiomeGenerationSettingsBuilder settings, RuleTest fillerType, BlockState state,
+                                    int veinSize, int minHeight, int maxHeight, int amount) {
+        settings.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                Feature.ORE.withConfiguration(new OreFeatureConfig(fillerType, state, veinSize))
+                        .withPlacement(Placement.RANGE.configure(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
+                        .square().count(amount));
+    }
 }
