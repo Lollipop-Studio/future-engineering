48be1f77c35a68fd56407a2c3d992a1d1a2400a9
bingling_sama <bingling_sama@qq.com>
2021-05-02 09:50:53 +0800

    Ores

--- src/main/java/moe/konara/fe/AllBlocks.java
+++ src/main/java/moe/konara/fe/AllBlocks.java
@@ -18,28 +18,24 @@ public class AllBlocks {
             COPPER_ORE = BLOCKS.register("copper_ore", () -> new OreBlock(
                     AbstractBlock.Properties
                             .create(Material.ROCK)
-                            .setRequiresTool()
                             .harvestTool(ToolType.PICKAXE)
                             .hardnessAndResistance(3.0F, 3.0F)
             )),
             ALUMINUM_ORE = BLOCKS.register("aluminum_ore", () -> new OreBlock(
                     AbstractBlock.Properties
                             .create(Material.ROCK)
-                            .setRequiresTool()
                             .harvestTool(ToolType.PICKAXE)
                             .hardnessAndResistance(3.0F, 3.0F)
             )),
             CERSEI_CRYSTAL_BLOCK = BLOCKS.register("cersei_crystal_block", () -> new OreBlock(
                     AbstractBlock.Properties
                             .create(Material.ROCK)
-                            .setRequiresTool()
                             .harvestTool(ToolType.PICKAXE)
                             .hardnessAndResistance(3.0F, 3.0F)
             )),
             GORAN_ORE = BLOCKS.register("goran_ore", () -> new OreBlock(
                     AbstractBlock.Properties
                             .create(Material.ROCK)
-                            .setRequiresTool()
                             .harvestTool(ToolType.PICKAXE)
                             .hardnessAndResistance(50.0F, 5.0F)
             ))
