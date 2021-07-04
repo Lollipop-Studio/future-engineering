efe475520de9768432bc5b2588cf7f9dc9742055
Thunder_luoling <thunder_luoling@outlook.com>
2021-07-04 10:20:35 +0800

    添加充能瑟维晶体、齿轮时钟

--- src/main/java/moe/konara/fe/items/AllItems.java
+++ src/main/java/moe/konara/fe/items/AllItems.java
@@ -10,6 +10,8 @@ import net.minecraft.item.Items;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.math.IEEE754rUtils;
+
 import static net.minecraft.item.Items.BUCKET;
 
 public class AllItems {
@@ -28,6 +30,8 @@ public class AllItems {
             ALUMINUM_INGOT = ITEMS.register("aluminum_ingot", () -> new Item(new Item.Properties().group(FE.FEGroup))),
             GORAN_GEM = ITEMS.register("goran_gem", () -> new Item(new Item.Properties().group(FE.FEGroup))),
             CERSEI_CRYSTAL = ITEMS.register("cersei_crystal", () -> new Item(new Item.Properties().group(FE.FEGroup))),
+            CHARGED_CERSEI_CRYSTAL = ITEMS.register("charged_cersei_crystal", () -> new Item(new Item.Properties().group(FE.FEGroup))),
+            GEAR_CLOCK = ITEMS.register("gear_clock", () -> new Item(new Item.Properties().group(FE.FEGroup))),
 
             //FluidBucket
             WEIRD_SPRING_WATER_BUCKET = ITEMS.register("weird_spring_water_bucket", () -> new BucketItem(AllFluids.WEIRD_SPRING_WATER, new Item.Properties().group(FE.FEGroup).containerItem(BUCKET)))
