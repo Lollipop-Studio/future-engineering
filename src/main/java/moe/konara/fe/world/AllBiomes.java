a2449f03d1474abdea0565c845407ba35150d284
Konara <imcharlesjiang@163.com>
2021-07-06 21:25:10 +0800

    生物群系加载

--- src/main/java/moe/konara/fe/world/AllBiomes.java
+++ src/main/java/moe/konara/fe/world/AllBiomes.java
@@ -10,6 +10,5 @@ public class AllBiomes {
     public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, FE.ID);
 
     public static RegistryObject<Biome>
-            MOONLIGHT = BIOMES.register("moonlightbiome", MoonLightBiome::get);
-            //TODO 人类至今未曾得知为什么崩溃      人間はなぜ彼らが崩壊したのかまだ知らない
+            MOONLIGHT = BIOMES.register("moonlightbiome", MoonLightBiome::make);
 }
--- src/main/java/moe/konara/fe/world/MoonLightBiome.java
+++ src/main/java/moe/konara/fe/world/MoonLightBiome.java
@@ -10,6 +10,18 @@ public class MoonLightBiome {
 
     private static Biome moonlight;
     public MoonLightBiome(){
+
+
+    }
+
+    private static int getSkyColorWithTemperatureModifier(float temperature) {
+        float lvt_1_1_ = temperature / 3.0F;
+        lvt_1_1_ = MathHelper.clamp(lvt_1_1_, -1.0F, 1.0F);
+        return MathHelper.hsvToRGB(0.62222224F - lvt_1_1_ * 0.05F, 0.5F + lvt_1_1_ * 0.1F, 1.0F);
+    }
+
+    public static Biome make(){
+        //TODO 未完成
         MobSpawnInfo.Builder mobspawninfo$builder = new MobSpawnInfo.Builder();
         DefaultBiomeFeatures.withSpawnsWithHorseAndDonkey(mobspawninfo$builder);
 
@@ -50,16 +62,6 @@ public class MoonLightBiome {
                 .withMobSpawnSettings(mobspawninfo$builder.build())
                 .withGenerationSettings(biomegenerationsettings$builder.build())
                 .build();
-
-    }
-
-    private static int getSkyColorWithTemperatureModifier(float temperature) {
-        float lvt_1_1_ = temperature / 3.0F;
-        lvt_1_1_ = MathHelper.clamp(lvt_1_1_, -1.0F, 1.0F);
-        return MathHelper.hsvToRGB(0.62222224F - lvt_1_1_ * 0.05F, 0.5F + lvt_1_1_ * 0.1F, 1.0F);
-    }
-
-    public static Biome get(){
         return moonlight;
     }
 }
